5f066c88891012de8c3e21d50cb2d5a166f3d2ed
Jim Brennan <jim.j.brennan@gmail.com>
2023-03-21 11:56:49 -0400

    passed 4 tests and got stuck

--- server/app.py
+++ server/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
-from models import db, Hero
+from models import db, Hero, Power, HeroPower
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
@@ -17,9 +17,130 @@ db.init_app(app)
 
 api = Api(app)
 
-@app.route('/')
-def home():
-    return ''
+# @app.route('/')
+# def home():
+#     return ''
+
+
+
+class Heroes(Resource):
+    def get(self):
+
+        heroes_list = [hero.to_dict() for hero in Hero.query.all()]
+
+        response = make_response(heroes_list, 200)
+
+        return response
+    
+api.add_resource(Heroes, '/heroes')
+
+class HeroById(Resource):
+    def get(self, id):
+        hero = Hero.query.filter(Hero.id == id).first().to_dict()
+
+        if not hero:
+            return make_response({
+                "error": "Hero not found"
+            }, 404)
+
+        response = make_response(hero, 200)
+
+        return response
+
+api.add_resource(HeroById, '/heroes/<int:id>')
+
+
+class Powers(Resource):
+    def get(self):
+
+        powers_list = [power.to_dict() for power in Power.query.all()]
+
+        response = make_response(powers_list, 200)
+
+        return response
+    
+api.add_resource(Powers, '/powers')
+
+class PowerById(Resource):
+    def get(self, id):
+        power = Power.query.filter(Power.id == id).first().to_dict()
+
+        if not power:
+            return make_response({
+                "error": "Power not found"
+            }, 404)
+
+        response = make_response(power, 200)
+
+        return response
+    
+
+    def patch(self, id):
+
+        power = Power.query.filter(Power.id == id).first()
+
+
+        # do i have to import abort??? 
+        if not power:
+            return make_response({
+                "error": "Power not found"
+            }, 404)
+
+        request_json = request.get_json()
+        for key in request_json():
+            setattr(power, key,request_json[key])
+        
+        db.session.add(power)
+        db.session.commit()
+
+        response = make_response(power.to_dict(), 200)
+
+        return response
+
+api.add_resource(PowerById, '/powers/<int:id>')
+
+
+# POST /hero_powers ....woof, not sure about this one but i'll try!
+# really need to go over Post...not sure about a lot of this one...
+# not sure where to put my error...or why not abort?
+
+class HeroPowers(Resource):
+    def get(self):
+
+        hero_powers_list = [hero_powers.to_dict() for hero_powers in HeroPower.query.all()]
+
+        response = make_response(hero_powers_list, 200)
+
+        return response
+    
+    def post(self):
+        request_json = request.get_json()
+
+
+        new_hero_power = HeroPower(
+            strength=request_json['strength'],
+            hero_id=request_json['hero_id'],
+            power_id=request_json['power_id']
+        )
+
+        if not new_hero_power:
+            return make_response({
+                "error": "Invalid input"
+            }, 404)
+
+        db.session.add(new_hero_power)
+        db.session.commit()
+
+
+        response = make_response(new_hero_power.to_dict(), 201)
+
+        return response
+
+
+
+
+api.add_resource(HeroPowers, '/hero_powers')
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,10 +1,75 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
 
-class Hero(db.Model):
-    __tablename__ = 'hero'
+db = SQLAlchemy(metadata=metadata)
+
+class Hero(db.Model, SerializerMixin):
+    __tablename__ = 'heroes'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    super_name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-# add any models you may need. 
+    hero_powers = db.relationship('HeroPower', back_populates='hero')
+
+    serialize_rules = ('-hero_powers', '-created_at', '-updated_at')
+
+    # honestly not sure if we need this, what it does or how to write it (might need to be a tuple if it's single argument)!
+    activities = association_proxy('hero_powers', 'powers')
+
+class Power(db.Model, SerializerMixin):
+    __tablename__ = 'powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    hero_powers = db.relationship('HeroPower', back_populates='power')
+
+    serialize_rules = ('-hero_powers', '-created_at', '-updated_at')
+
+    @validates('description')  
+    def validate_description(self, key, description):
+        if not description:
+            raise ValueError('the description must be at least 20 characters')
+        
+        if len(description) < 20:
+            raise ValueError('the description must be at least 20 characters')
+        return description
+
+
+class HeroPower(db.Model, SerializerMixin):
+    __tablename__ = 'hero_powers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    hero_id = db.Column(db.Integer, db.ForeignKey('heroes.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('powers.id'))
+
+    hero = db.relationship('Hero', back_populates='hero_powers')
+    power = db.relationship('Power', back_populates='hero_powers')
+
+    serialize_rules = ('-hero.hero_powers', '-power.hero_powers', '-created_at', '-updated_at')
+
+
+
+    # not 100% sure this is how you write this if statement!
+    @validates('strength')  
+    def validate_strength(self, key, strength):
+        if strength != 'Strong' and strength != 'Weak' and strength != 'Average':
+            raise ValueError('that is not a valid strength rating')
+        return strength
